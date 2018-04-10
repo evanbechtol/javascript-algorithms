18127577fc35aba4cef9ef4cb02cfb9371ace97c
eevabec <evan.bechtol@ericsson.com>
2018-04-10 14:32:26 -0500

    Add JSDoc for `randomNumber` method

--- util/index.js
+++ util/index.js
@@ -1,4 +1,10 @@
 module.exports = {
+  /**
+   * @description Generate a random number between max, and min
+   * @param max {int} Integer to represent maximum value generated number can have
+   * @param min {int} Integer to represent minimum value generated number can have
+   * @returns {number} Returns a randomly generated number between the max and min provided
+   */
   randomNumber : ( max = 100, min = 0 ) => {
     return Math.floor( ( Math.random() * max ) + min );
   }
