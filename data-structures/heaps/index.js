4e0090a1d2f1375f38a05f26782cc2d87d2930db
eevabec <evan.bechtol@ericsson.com>
2018-04-24 21:50:55 -0500

    Binary Heap
    - Fix issue where maxHeapify would not correctly calculate left or
      right child for nodes where %3 === 0

--- data-structures/heaps/index.js
+++ data-structures/heaps/index.js
@@ -1,42 +1,44 @@
-function Heap () {
-  let data = [],
+const assert = require( 'assert' );
 
+function Heap ( arr = [] ) {
+  this.data     = arr;
+  this.heapSize = this.data.length;
 }
 
 
 Heap.prototype.buildMaxHeap = function () {
-
+  this.heapSize = this.data.length;
+  for ( let i = Math.floor( this.data.length / 2 ); i >= 0; i-- ) {
+    console.log( `maxHeapify with idx ${i}` );
+    this.maxHeapify( this.data, i );
+  }
 };
 
-
-Heap.prototype.maxHeapify = function () {
-  this.data = this._maxHeapify( this.data, 0 );
-};
-
-Heap.prototype._maxHeapify = function ( arr, idx ) {
+Heap.prototype.maxHeapify = function ( arr, idx ) {
   let left  = this.left( idx ),
       right = this.right( idx ),
       largest;
-
-  if ( left < arr.length && arr[ left ] > arr[ idx ] ) {
+  console.log( `arr[${idx}] = ${arr[ idx ]} has arr[${left}] = ${arr[ left ]} and arr[${right}] = ${arr[ right ]}` );
+  //console.log( `maxHeapify with idx ${idx}` );
+  if ( left <= this.heapSize && arr[ left ] > arr[ idx ] ) {
     largest = left;
   } else {
     largest = idx;
   }
 
-  if ( right < arr.length && arr[ right ] > arr[ largest ] ) {
+  if ( right <= this.heapSize && arr[ right ] > arr[ largest ] ) {
     largest = right;
   }
-
+  console.log( `Largest is index ${largest} with value ${arr[ largest ]}` );
   if ( largest !== idx ) {
     let temp = arr[ idx ];
-
+    console.log( `Old array: ${arr}` );
+    console.log( `swapping ${arr[ idx ]} with ${arr[ largest ]}` );
     arr[ idx ]     = arr[ largest ];
     arr[ largest ] = temp;
-    this._maxHeapify( arr, largest );
+    console.log( `New array: ${arr}` );
+    this.maxHeapify( arr, largest );
   }
-
-  return arr;
 };
 
 
@@ -46,10 +48,30 @@ Heap.prototype.parent = function ( idx ) {
 
 
 Heap.prototype.left = function ( idx ) {
+  if ( idx === 0 || idx % 3 === 0 ) {
+    return ( 2 * idx ) + 1;
+  }
   return idx ? 2 * idx : null;
 };
 
 
 Heap.prototype.right = function ( idx ) {
+  if ( idx === 0 || idx % 3 === 0 ) {
+    return ( 2 * idx ) + 2;
+  }
   return idx ? ( 2 * idx ) + 1 : null;
 };
+
+
+function main () {
+  let arr      = [ 16, 4, 10, 14, 7, 9, 3, 2, 8, 1 ],
+      expected = [ 16, 14, 10, 8, 7, 9, 3, 2, 4, 1 ],
+      heap     = new Heap( arr );
+
+  console.log( heap.data );
+  console.log( heap.maxHeapify( heap.data, 1 ) );
+  console.log( heap.data );
+  console.log( expected );
+}
+
+main();
