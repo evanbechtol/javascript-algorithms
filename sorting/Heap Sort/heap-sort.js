bc80867e293eb77df417c4f84b7584e35dcaf42a
evan bechtol <evanbechtol@gmail.com>
2020-01-26 16:41:54 -0600

    Heap Sort Documentation
    - Document `parent`
    - Document `left`
    - Document `right`

--- sorting/Heap Sort/heap-sort.js
+++ sorting/Heap Sort/heap-sort.js
@@ -1,11 +1,26 @@
+/**
+ * @description Retrieves the parent for the provided index
+ * @param i {number} Index to return the parent of
+ * @return {number} Index of parent
+ */
 function parent ( i ) {
   return Math.floor( i / 2 );
 }
 
+/**
+ * @description Retrieves the left child for the provided index
+ * @param i {number} Index to return the left child of
+ * @return {number} Index of left child
+ */
 function left ( i ) {
   return 2 * i + 1;
 }
 
+/**
+ * @description Retrieves the right child for the provided index
+ * @param i {number} Index to return the right child of
+ * @return {number} Index of right child
+ */
 function right ( i ) {
   return 2 * i + 2;
 }
