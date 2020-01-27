e7dfecaf060d47bcc6d536c620658efcb6ed09ec
eevabec <evan.bechtol@ericsson.com>
2020-01-27 16:47:46 -0600

    Stack Implementation

--- data-structures/stacks/index.js
+++ data-structures/stacks/index.js
@@ -1,14 +1,46 @@
+/**
+ * @description Implements the stack data structure
+ */
 class Stack {
-  constructor() {
-
+  constructor () {
+    this.stack = [];
+    this.top = this.stack.length;
   }
 
-  push() {
+  push ( elem ) {
+    if ( !elem ) {
+      throw new Error( "Undefined data cannot be inserted into stack" );
+    }
 
+    this.stack.push( elem );
+    this.setTop(this.stack.length);
   }
 
-  pop() {
+  pop () {
+    if ( this.isEmpty() ) {
+      throw new Error( "Stack underflow" );
+    }
 
+    let value = this.stack.pop();
+    this.setTop(this.stack.length);
+
+    return value;
+  }
+
+  getTop () {
+    return this.stack[this.top - 1];
+  }
+
+  setTop ( value ) {
+    this.top = value;
+  }
+
+  getStack() {
+    return this.stack;
+  }
+
+  isEmpty () {
+    return this.top === 0;
   }
 }
 
--- data-structures/stacks/test.js
+++ data-structures/stacks/test.js
@@ -7,12 +7,84 @@ describe( "Stacks", () => {
   const stack = new Stack();
 
   describe( "Exposed methods", () => {
-    it( "Push", () => {
+    it( "push", () => {
       assert.isFunction( stack.push, "Method 'push' does not exist" );
     } );
 
-    it( "Pop", () => {
+    it( "pop", () => {
       assert.isFunction( stack.pop, "Method 'pop' does not exist" );
+    } );
+
+    it( "isEmpty", () => {
+      assert.isFunction( stack.isEmpty, "Method 'isEmpty' does not exist" );
+    } );
+
+    it( "getTop", () => {
+      assert.isFunction( stack.getTop, "Method 'getTop' does not exist" );
+    } );
+
+    it( "setTop", () => {
+      assert.isFunction( stack.setTop, "Method 'setTop' does not exist" );
+    } );
+
+    it( "getStack", () => {
+      assert.isFunction( stack.getStack, "Method 'getStack' does not exist" );
+    } );
+  } );
+
+  describe( "Initialization", () => {
+    it( "Should initialize empty array", () => {
+      assert.deepEqual( stack.getStack().length, 0, "Array does not have length 0" );
+    } );
+
+    it( "Should have top value of 0", () => {
+      assert.deepEqual( stack.top, 0, "Top is not initialized to 0" );
+    } );
+
+    it( "Should return isEmpty === true", () => {
+      assert.deepEqual( stack.isEmpty(), true, "isEmpty did not return true" );
+    } );
+
+    it( "Should throw error calling pop", () => {
+      try {
+        stack.pop();
+      } catch ( err ) {
+        assert.equal( err.message, "Stack underflow", "Error message incorrect" );
+      }
+    } );
+  } );
+
+  describe( "Operations", () => {
+    it( "Should not push invalid data", () => {
+      try {
+        stack.push( undefined );
+      } catch ( err ) {
+        assert.equal( err.message, "Undefined data cannot be inserted into stack", "Error message incorrect" );
+      }
+    } );
+
+    it( "Should push element", () => {
+      assert.isUndefined( stack.push( 1 ), "Call to push threw error" );
+    } );
+
+    it( "Should update top to 1", () => {
+      assert.deepEqual( stack.getTop(), 1, "Top not updated" );
+    } );
+
+    it( "Should return stack with length 1", () => {
+      assert.equal( stack.getStack().length, 1, "Stack length incorrect" );
+    } );
+
+    it ("Should return element at top of stack", () => {
+      assert.deepEqual( stack.getTop(), 1, "Returned incorrect element" );
+    });
+
+    it( "Should pop element and return it", () => {
+      assert.deepEqual( stack.pop(), 1, "Incorrect result returned" );
+    } );
+
+    it( "Should update top to 0", () => {
+      assert.deepEqual( stack.top, 0, "Top not updated" );
     } );
   } );
 } );
