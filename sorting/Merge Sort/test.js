2865a783856b56a33f18b5dbaed2a446c0415c5a
eevabec <evan.bechtol@ericsson.com>
2020-02-04 09:46:28 -0600

    Update Merge Sort tests
    - Reduce time taken by storing sorted array outside of test

--- sorting/Merge Sort/test.js
+++ sorting/Merge Sort/test.js
@@ -4,10 +4,15 @@ const { mergeSort } = require( "./merge-sort" );
 const utils = require( "../../util" );
 
 describe( "Merge Sort", () => {
-  const length = 10000;
+  const length = 1000000;
   const arr = utils.generateArray( length, length );
+  const sortedArr = arr.sort( utils.compare );
 
   it( "Should sort array", () => {
-    assert.deepEqual( mergeSort( arr, "asc" ), arr.sort(utils.compare), `Arrays are not sorted identically` );
+    assert.deepEqual( mergeSort( arr, "asc" ), sortedArr, `Arrays are not sorted identically` );
   } );
+
+  it ( "Should check native sort", () => {
+    assert.deepEqual( arr.sort(), sortedArr, `Arrays are not sorted identically` );
+  });
 } );
