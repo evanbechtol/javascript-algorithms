87f3730768506765f41cabf2e210343895d9aab6
eevabec <evan.bechtol@ericsson.com>
2020-02-04 10:25:08 -0600

    Update Merge and Quick Sort Tests
    - Increase number of elements tested
    - Utilize `utils.compare` in native sort for accuracy
    - Fix issue where array was being sorted ahead of time, causing quicksort result inaccuracies

--- sorting/Merge Sort/test.js
+++ sorting/Merge Sort/test.js
@@ -4,15 +4,16 @@ const { mergeSort } = require( "./merge-sort" );
 const utils = require( "../../util" );
 
 describe( "Merge Sort", () => {
-  const length = 1000000;
+  const length = 10000;
   const arr = utils.generateArray( length, length );
-  const sortedArr = arr.sort( utils.compare );
+  let sortedArr = Array.from( arr );
+  sortedArr.sort( utils.compare );
 
   it( "Should sort array", () => {
     assert.deepEqual( mergeSort( arr, "asc" ), sortedArr, `Arrays are not sorted identically` );
   } );
 
-  it ( "Should check native sort", () => {
-    assert.deepEqual( arr.sort(), sortedArr, `Arrays are not sorted identically` );
-  });
+  it( "Should check native sort", () => {
+    assert.deepEqual( arr.sort( utils.compare ), sortedArr, `Arrays are not sorted identically` );
+  } );
 } );
--- sorting/Quick Sort/test.js
+++ sorting/Quick Sort/test.js
@@ -2,6 +2,7 @@ const assert = require( "chai" ).assert;
 const mocha = require( "mocha" );
 const quickSort = require( "./quick-sort" );
 const { generateArray } = require( "../../util" );
+const utils = require( "../../util" );
 
 describe( "Quick Sort", () => {
   describe( "Exposed methods", () => {
@@ -11,11 +12,17 @@ describe( "Quick Sort", () => {
   } );
 
   describe( "quickSort", () => {
-    const length = 100000;
-    let arr = generateArray( length );
+    const length = 500000;
+    const arr = generateArray( length, length );
+    let sortedArr = Array.from( arr );
+    sortedArr.sort( utils.compare );
 
     it( "Should sort array", () => {
-      assert.deepEqual( quickSort.quickSort( arr, 0, arr.length - 1, arr.length ), arr.sort(), `Array not sorted properly` );
+      assert.deepEqual( quickSort.quickSort( arr, 0, arr.length - 1, arr.length ), sortedArr, `Array not sorted properly` );
     } );
+
+    it ( "Should check native sort", () => {
+      assert.deepEqual( arr.sort( utils.compare ), sortedArr, `Arrays are not sorted identically` );
+    });
   } );
 } );
